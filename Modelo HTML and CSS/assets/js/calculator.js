c561fccf0eef9b069d2981711577d0791fe2e01a
correa2020 <correa.l@icloud.com>
2022-06-16 19:54:09 -0300

    Ajuste nos comentarios

--- Modelo HTML and CSS/assets/js/calculator.js
+++ Modelo HTML and CSS/assets/js/calculator.js
@@ -1,77 +1,80 @@
-function createCalculator() {
+function createCalculator() {                                                               //OBJETO CALCULADORA
     return {
-        display: document.querySelector(".display"),
+        display: document.querySelector(".display"),                                        //SELECIONA O INPUT
 
-        start() {
-            this.btnClick();
-            this.enterClick();
+        start() {                                                                           //UM ATRIBUTO DO OBJETO É A FUNÇÃO INICIAR
+            this.btnClick();                                                                //CHAMA A FUNÇÃO CLICK DO BOTAO
+            this.enterClick();                                                              //CHAMA A FUNÇÃO ENTER
         },
 
-        result() {
-            let operation = this.display.value;
+        result() {                                                                          //UM ATRIBUTO DO OBJETO É A FUNÇÃO RESULADO
+            let operation = this.display.value;                                             //CRIA UMA VARIAVEL QUE ARMAZENA A OPERAÇÃO DO DISPLAY
 
-        try {
-            calculation = eval(operation);
+            try {                                                                           //TENTE
+                const calculation = eval(operation);                                        //ARMAZENAR UM EVAL DE OPERATION NUMA CONSTANTE CHAMADA CALCULATION
 
-            if(!calculation) {
+                if(!calculation) {                                                          //SE DER ERRADO EXIBE UM ALERTA
+                    alert("Conta Inválida");
+                    return;
+                }
+
+                this.display.value = String(calculation);                                   //O DISPLAY RECEBE O RESULTADO
+            } catch(erro) {                                                                 //O CASO DE ERRO É EXIBIDO UM ALERTA
                 alert("Conta Inválida");
                 return;
             }
-
-            this.display.value = String(calculation);
-        } catch(erro) {
-            alert("Conta Inválida");
-            return;
-        }
-
         },
 
-        clearDisplay() {
-            this.display.value = "";
+        clearDisplay() {                                                                    //CRIA FUNÇÃO PARA LIMAPR O DISPLAY
+            this.display.value = "";                                                        //DISPLAY RECEBE O VALOR VAZIO
         },
 
-        deleteLast() {
-            this.display.value = this.display.value.slice(0, -1);
+        deleteLast() {                                                                      //FUNÇÃO PARA DELETAR O ULTIMO
+            this.display.value = this.display.value.slice(0, -1);                           //O DISPLAY RECEBE UM SLICE DE TODOS MENOS 1 OU SEJA PEGA TODOS VALORES E TIRA O ULTIMO
         },
 
-        enterClick() {
-            this.display.addEventListener("keyup", event => {
-                if(event.keyCode === 13) {
-                    this.result();
+        enterClick() {                                                                      //FUNÇÃO PARA O CLICK NO ENTER
+            this.display.addEventListener("keyup", event => {                               //CAPTURA O EVENTO DE CLICK DO DISPLAY
+                if(event.keyCode === 13) {                                                  //SE O EVENTO CAPTURADO FOR 13
+                    this.result();                                                          //CHAMA A VARIAVEL RESULTADO
                 }
             });
         },
 
-        btnClick() {
-            document.addEventListener("click", event => {
-                const element = event.target;
-
-                if(element.classList.contains("btn-caracter")) {
-                    this.btnToDisplay(element.innerText);
-                }
-
-                if(element.classList.contains("btn-clear")) {
-                    this.clearDisplay();
-                }
-
-                if(element.classList.contains("btn-backspace")) {
-                    this.deleteLast();
-                }
-
-                if(element.classList.contains("btn-equal")) {
-                    this.result();
-                }
-
-            });                                                          //Outra opção seria utilizar a função .bind(this)
-        },
-
-        btnToDisplay(value) {
-            this.display.value += value;
+        btnToDisplay(value) {                                                               //FUNÇÃO QUE ENVIA O VALOR PARA O DISPLAU
+            this.display.value += value;                                                    //DISPLAY RECEBE OS DADOS QUE JA TEM MAIS O VALOR
         }
 
+
+        btnClick() {                                                                        //CAPTURA O EVENTO DE CLICK DO DOCUMENT
+            document.addEventListener("click", event => {
+                const element = event.target;                                               //ARMAZENA ESSE CLICK EM ELEMENT
+
+                if(element.classList.contains("btn-caracter")) {                            //CASO O ELEMENT TENHA A VARIAVEL BTN-CARACTER
+                    this.btnToDisplay(element.innerText);                                   //CHAMA A FUNÇÃO COM O DADO DO ELEMENT
+                    this.display.focus();                                                   //DA FOCUS NO DISPLAY
+                }
+
+                if(element.classList.contains("btn-clear")) {                               //CASO O ELEMENT TENHA A VARIAVEL BTN-CLEAR
+                    this.clearDisplay();                                                    //CHAMA A FUNÇÃO DE LIMPAR O DISPLAY
+                    this.display.focus();                                                   //DA FOCUS NO DISPLAY
+                }
+
+                if(element.classList.contains("btn-backspace")) {                           //CASO O ELEMENT TENHA A VARIAVEL BTN-BACKSPACE
+                    this.deleteLast();                                                      //CHAMA A FUNÇÃO DE DELETAR O ULTIMO
+                    this.display.focus();                                                   //DA FOCUS NO DISPLAY
+                }
+
+                if(element.classList.contains("btn-equal")) {                               //CASO O ELEMENT TENHA A VARIAVEL BTN-EQUAL
+                    this.result();                                                          //CHAMA A FUNÇÃO DE RESULTADO
+                    this.display.focus();                                                   //DA FOCUS NO DISPLAY
+                }
+
+            });
+        },
 
     };
 }
 
-const calculator = createCalculator();
-calculator.start();
+const calculator = createCalculator();                                                      //ARMAZENA A FUNÇÃO EM UMA VARIAVEL
+calculator.start();                                                                         //CHAMA O ATRIBUTO START DO OBJETO CALCULATOR QUE POR SUA VEZ É UMA FUNÇÃO
