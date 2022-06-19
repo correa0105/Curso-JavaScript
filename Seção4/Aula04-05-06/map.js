4650e516e87c6a91c002ebfe4692bfc55f628acf
correa2020 <correa.l@icloud.com>
2022-06-18 21:38:35 -0300

    Aula map

---
+++ Seção4/Aula04-05-06/map.js
@@ -0,0 +1,29 @@
+//DOBRE OS NUMEROS DO ARRAY
+
+const numbers = [30, 6, 2, 3, 26, 32, 9, 8, 15, 28];
+const doubleNumbers = numbers.map(value => value*2);
+
+console.log(doubleNumbers);
+
+//RETORNE UMA STRING COM O NOME DA PESSOA, REMOVA A CHAVE NAME DO OBJETO ADICIONA UMA CHAVE ID EM CADA OBJETO;
+
+const people = [ 
+    { name: "Lucas", year: 62 },
+    { name: "Carla", year: 32 },
+    { name: "Guilherme", year: 20 },
+    { name: "Andressa", year: 17 },
+    { name: "Joao", year: 19 },
+    { name: "Micaela", year: 37 },
+];
+
+const arrayNames = people.map(value => value.name);
+
+console.log(arrayNames);
+ 
+const objectNoKey = people.map(object => ({year: object.year}));
+
+console.log(objectNoKey);
+
+const addIdToObject = people.map((object, index) => {object.Id = index; return object});
+
+console.log(addIdToObject);
