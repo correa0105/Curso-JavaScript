2f5b173179f99cb62935af4182dcf36d9f763519
correa2020 <correa.l@icloud.com>
2022-06-22 04:31:28 -0300

    Aula de prototype

---
+++ Seção5/Aula04-05-06/prototype.js
@@ -0,0 +1,49 @@
+function People(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    /* this.fullName = () => "Original Construtor: " + this.firstname + " " + this.lastname; */
+}
+
+People.prototype.fullName = function() {return this.firstname + " " + this.lastname};
+
+const people1 = new People("Lucas", "Corrêa");
+
+console.log(people1.fullName());
+
+/////////////////////////////////////////////////////
+
+const objA = {
+    keyA: "A",
+}
+
+const objB = {
+    keyB: "B",
+}
+
+const objC = new Object();
+objC.keyC = "C";
+
+Object.setPrototypeOf(objB, objA);
+Object.setPrototypeOf(objC, objB);
+
+console.log(objC.keyB);
+
+/////////////////////////////////////////////////////
+
+function Product(nameproduct, price) {
+    this.nameproduct = nameproduct;
+    this.price = price;
+}
+
+Product.prototype.discount = function(percent) {
+    this.price = this.price - (this.price * (percent / 100));
+}
+
+Product.prototype.increases = function(percent) {
+    this.price = this.price + (this.price * (percent / 100));
+}
+
+const product1 = new Product("T-Shirt", 50);
+
+product1.discount(20);
+console.log(product1);
